43a21b34fea59234e21b4555538eefa013cfbf13
Sanjay <sanjay@Sanjays-MacBook-Pro.local>
2022-12-17 12:34:01 +0530

    test eslint prettier husky 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
   parserOptions: {
     parser: 'babel-eslint',
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
   plugins: [],
-  // add your custom rules here
+  //custom rules here
   rules: {
     'react/prop-types': 1,
     'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': 0,
+    'no-console': 1,
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ name }) {
   console.log('kk');
   let x = 100;
 
