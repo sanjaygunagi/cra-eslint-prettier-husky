09418e1d9f1a9602917eea7da61237ee48aec0ae
Sanjay <sanjay@Sanjays-MacBook-Pro.local>
2022-12-19 11:55:31 +0530

    test

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App({ name }) {
   let x = 100;
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a href='#'>hello</a>
+        <a href="#">hello</a>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
