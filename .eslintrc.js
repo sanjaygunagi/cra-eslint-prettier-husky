58a2af7332f0d8a937d29f2625318f3f70e5ed91
Sanjay <sanjay@Sanjays-MacBook-Pro.local>
2022-12-19 14:27:30 +0530

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,13 @@ module.exports = {
     sourceType: 'module',
   },
   // extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+  ],
   plugins: [],
   // custom rules here
   rules: {
--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,3 +1,8 @@
 export const Test = () => {
-  return <h1>Test</h1>;
+  return (
+    <div>
+      <h1>Test</h1>
+      <img src='' />
+    </div>
+  );
 };
