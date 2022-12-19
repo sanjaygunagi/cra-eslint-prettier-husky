88956252c8963dc0ecc6648d06219f9df5621fa8
Sanjay <sanjay@Sanjays-MacBook-Pro.local>
2022-12-19 12:07:50 +0530

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,15 @@ module.exports = {
   parserOptions: {
     sourceType: 'module',
   },
-  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  // extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:react/recommended'],
   plugins: [],
   // add your custom rules here
   rules: {
     'react/prop-types': 1,
     'react/react-in-jsx-scope': 'off',
     'no-console': 1,
-    'no-unused-vars': [
-      'error',
-      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
-    ],
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
   },
   settings: {
     react: {
